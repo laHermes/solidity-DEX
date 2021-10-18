ef5bf42779e8885f0f2f04ef3f5e8f7a5050cd88
Lazar Jankovic <jlazar997@gmail.com>
2021-10-18 17:59:29 +0200

    add init functions

--- contracts/exchange/dex.sol
+++ contracts/exchange/dex.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract DEX {
 
-
+    IERC20 token;
     uint256 public totalLiquidity;
     mapping (address=>uint256) tokenLiquidity;
 
@@ -14,12 +14,21 @@ contract DEX {
         _;
     }
 
-    function initialize(address _tokenAddress) firstTimeOnly payable external {
-        
-
+    function initialize(address _tokenAddress, uint256 _tokenAmount) firstTimeOnly payable external {
+        totalLiquidity = msg.value;
+        tokenLiquidity[msg.sender] = totalLiquidity;
+        require(token(_tokenAddress).transferForom(smg.sender, address(this), _tokenAmount);        
     }
-    function ethToToken() external payable{
 
+    function tokenPrice(uint256 _amountRequested, uint256 _reserveLevels) public returns(uint256){
+    
+    }
+
+    function ethToToken(address _tokenAddress) external payable{
+        uint256 reserve = token(_tokenAddress).balanceOf(this(address));
+        //determine token price
+        uint256 tokenAmount = price()
+        request(token(_tokenAddress).transfer(msg.sender, msg.sender), "Transaction Failed!");
     }
 
     function tokenToEth() external{

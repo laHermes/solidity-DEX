956a76b62005cae38a5e001b2f58c864495f0193
Lazar Jankovic <jlazar997@gmail.com>
2022-01-03 15:18:05 +0100

    add netspec comments

--- contracts/exchange/dex.sol
+++ contracts/exchange/dex.sol
@@ -1,29 +1,23 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./LPToken.sol";
 import "./DexFactory.sol";
 
 /// @title Solidity-swap
 /// @author Lazar
 /// @notice This is a personal project with the goal of better understanding AMM
 /// @notice This contract is not tested and is currently in dev phase DO NOT USE IT!
-contract Dex is ERC20 {
+
+contract Dex is LPToken {
     /* ========== STATE VARIABLES ========== */
     IERC20 public token;
     DexFactory public factory;
 
     uint256 public invariant;
-    uint256 public totalSupplySwap;
     uint256 public totalLiquidity;
-    uint256 public rewardRatePerBlock = 100;
-    uint256 public rewardPerTokenAmount;
-    uint256 public lastUpdateTime;
 
     mapping(address => uint256) public tokenLiquidity;
-    mapping(address => uint256) public tokensStaked;
-    mapping(address => uint256) public userRewardPerTokenPaid;
-    mapping(address => uint256) rewards;
 
     /* ========== FALLBACKS & MODIFIERS ========== */
 
@@ -33,26 +27,19 @@ contract Dex is ERC20 {
 
     receive() external payable {}
 
-    modifier updateRewards() {
-        lastUpdateTime = block.timestamp;
-        rewardPerTokenAmount = rewardPerToken();
-        rewards[msg.sender] = earned(msg.sender);
-        userRewardPerTokenPaid[msg.sender] = rewardPerTokenAmount;
-        _;
-    }
-
     /* ========== CONSTRUCTOR ========== */
 
-    /// Constructor
-    /// @param _tokenAddress ERC20 contract address
-    /// @param _tokenAmount amount of ERC20 tokens
-    /// @dev msg.value amount of ETH to be pooled
-    /// @dev invariant = msg.value * _tokenAmount
-    /// @dev LP token amount is a percentage (share) of total liquidity provided by user
-    /// @dev LP token amount = tokenLiquidity[msg.sender] / totalLiquidity
+    /**
+     @param _tokenAddress ERC20 contract address    
+     @param _tokenAmount amount of ERC20 tokens    
+     @dev msg.value amount of ETH to be pooled    
+     @dev invariant = msg.value * _tokenAmount    
+     @notice LP token amount is a percentage (share) of total liquidity provided by user    
+     @dev LP token amount = tokenLiquidity[msg.sender] / totalLiquidity
+     **/
     constructor(address _tokenAddress, uint256 _tokenAmount)
         payable
-        ERC20("LPDex", "LPD")
+        LPToken("LPDex", "LPD")
     {
         require(totalLiquidity == 0, "DEX::initialize: already init!");
         require(
@@ -78,18 +65,12 @@ contract Dex is ERC20 {
 
     /* ========== FUNCTIONS ========== */
 
-    function earned(address _account) public view returns (uint256) {
-        return
-            ((tokensStaked[_account] *
-                (rewardPerToken() - userRewardPerTokenPaid[_account])) / 1e18) +
-            rewards[_account];
-    }
-
-    /// Return amount of tokens to be received.
-    /// @param _inputTokenAmount amount of tokens to be swapped for target token
-    /// @param _inputTokenReserve current reserves of  input token (ETH or ERC20)
-    /// @param _outputTokenReserve current reserves of output token (ETH or ERC20)
-    /// @return amount of tokens that can be swapped for a given input amount
+    /**
+    @param _inputTokenAmount amount of tokens to be swapped for target token
+    @param _inputTokenReserve current reserves of  input token (ETH or ERC20)
+    @param _outputTokenReserve current reserves of output token (ETH or ERC20)
+    @return amount of tokens that can be swapped for a given input amount
+    **/
     function tokenPrice(
         uint256 _inputTokenAmount,
         uint256 _inputTokenReserve,
@@ -97,16 +78,6 @@ contract Dex is ERC20 {
     ) public view returns (uint256) {
         uint256 yToken = invariant / (_inputTokenReserve + _inputTokenAmount);
         return _outputTokenReserve - yToken;
-    }
-
-    function rewardPerToken() public view returns (uint256) {
-        if (totalSupplySwap == 0) {
-            return 0;
-        }
-
-        return
-            (block.timestamp - lastUpdateTime * rewardRatePerBlock * 1e18) /
-            totalSupplySwap;
     }
 
     function ethToToken(uint256 _inputEth) private returns (uint256) {
@@ -124,10 +95,16 @@ contract Dex is ERC20 {
         return tokenAmount;
     }
 
+    /**
+    @dev a router function
+    **/
     function ethToTokenSwap() external payable returns (uint256) {
         return ethToToken(msg.value);
     }
 
+    /**
+    @dev swap ERC20 token for ETH 
+    **/
     function tokenToEth(uint256 _tokenAmount) external returns (uint256) {
         uint256 tokenReserve = token.balanceOf(address(this));
         uint256 tokenSwapPrice = tokenPrice(
@@ -144,6 +121,9 @@ contract Dex is ERC20 {
         return tokenSwapPrice;
     }
 
+    /**
+    @dev deposit ERC20 token along with ETH
+    **/
     function deposit() external payable updateRewards {
         require(
             msg.value > 0,
@@ -162,6 +142,9 @@ contract Dex is ERC20 {
         );
     }
 
+    /**
+    @dev Withdraw ERC20 token along with ETH
+    **/
     function withdraw(uint256 _amount) external payable updateRewards {
         require(_amount > 0, "Dex::withdraw: amount must be greater than 0!");
         uint256 tokenBalance = token.balanceOf(address(this));
